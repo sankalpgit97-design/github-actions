11ccf3872cfd3e91d0b1d260b1280a5076ced47d
sankalpgit97-design <sankalpgit97@gmail.com>
2026-08-30 01:03:43 +0530

    test cases commit 3

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -19,23 +19,4 @@ test.describe('Login Test Automation - Login Flow', () => {
     await expect(page.locator('text=Log out')).toBeVisible();
     });
 
-    test('Negative username test', async ({ page }) => {
-        await page.locator('#username').fill(loginData.incorrectUser);
-        await page.locator('#password').fill('Password123');
-        await page.locator('#submit').click();
-
-    const error = page.locator('#error');
-    await expect(error).toBeVisible();
-    await expect(error).toHaveText('Your username is invalid!');
-    });
-
-    test('Negative password test', async ({ page }) => {
-        await page.locator('#username').fill('student');
-        await page.locator('#password').fill(loginData.incorrectPassword);
-        await page.locator('#submit').click();
-
-        const error = page.locator('#error');
-        await expect(error).toBeVisible();
-        await expect(error).toHaveText('Your password is invalid!');
-    });
 });
