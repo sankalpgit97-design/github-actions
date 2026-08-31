b02c4c6a06b9f828e394a4b8696979045254be73
sankalpgit97-design <sankalpgit97@gmail.com>
2026-08-31 12:13:29 +0530

    test commit 4

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,22 +1,38 @@
 const {test, expect} = require('@playwright/test');
-const loginData=require("../data/loginData.json");
-test.describe('Login Test Automation - Login Flow', () => {
+const {LoginPage} = require('../pages/LoginPage')
+const testData = require('../config.json');
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto(loginData.url);
-    });
+test ('Log in and save session state', async ({page}) => {
+const loginPage = new LoginPage(page);
 
-    test('Positive Login Test', async ({ page }) => {
-        const username = process.env.USER_NAME;
-        const password = process.env.PASSWORD;
+await loginPage.login(testData.url, testData.username, testData.password);
+
+await page.context().storageState({path: 'state.json'});
+
+}
+);
+// const loginData=require("../data/loginData.json");
+// test.describe('Login Test Automation - Login Flow', () => {
+
+//     test.beforeEach(async ({ page }) => {
+//         await page.goto(loginData.url);
+//     });
+
+//     test('Positive Login Test', async ({ page }) => {
+//         const username = process.env.USER_NAME;
+//         const password = process.env.PASSWORD;
         
-    await page.locator('#username').fill(loginData.username);
-    await page.locator('#password').fill(loginData.password);
-    await page.locator('#submit').click();
+//     await page.locator('#username').fill(loginData.username);
+//     await page.locator('#password').fill(loginData.password);
+//     await page.locator('#submit').click();
 
-    await expect(page).toHaveURL(/.*logged-in-successfully/);
-    await expect(page.locator('text=successfully logged in')).toBeVisible();
-    await expect(page.locator('text=Log out')).toBeVisible();
-    });
+//     await expect(page).toHaveURL(/.*logged-in-successfully/);
+//     await expect(page.locator('text=successfully logged in')).toBeVisible();
+//     await expect(page.locator('text=Log out')).toBeVisible();
+//     });
 
-});
+
+// });
+
+
+const {test} = require
