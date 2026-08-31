4ceba5af81a0b8c534ced1bb403496a7d96c7e95
sankalpgit97-design <sankalpgit97@gmail.com>
2026-08-31 13:15:39 +0530

    test commit PW 6.1

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,10 +2,10 @@ const {test, expect} = require('@playwright/test');
 const loginData=require("../data/loginData.json");
 
 test('Sucessfull Login Test using JSON Data' ,async ({page}) => {
-await page.goto(testData.url);
+await page.goto(loginData.url);
 
-await page.locator('#username').fill(testData.username);
-await page.locator('#password').fill(testData.password);
+await page.locator('#username').fill(loginData.username);
+await page.locator('#password').fill(loginData.password);
 await page.locator('#submit').click();
 
 
