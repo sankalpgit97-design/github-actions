380f722cc716419abe592f0dcaf8ea5dc00618e5
sankalpgit97-design <sankalpgit97@gmail.com>
2026-08-31 13:08:05 +0530

    test commit PW 6

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,35 +1,20 @@
 const {test, expect} = require('@playwright/test');
-const {loginPage} = require('../pages/loginPage')
-const testData = require('../data/loginData.json');
+const loginData=require("../data/loginData.json");
 
-test ('Log in and save session state', async ({page}) => {
-const loginPage = new LoginPage(page);
+test('Sucessfull Login Test using JSON Data' ,async ({page}) => {
+await page.goto(testData.url);
 
-await loginPage.login(testData.url, testData.username, testData.password);
-
-await page.context().storageState({path: 'state.json'});
-
-}
-);
-// const loginData=require("../data/loginData.json");
-// test.describe('Login Test Automation - Login Flow', () => {
-
-//     test.beforeEach(async ({ page }) => {
-//         await page.goto(loginData.url);
-//     });
-
-//     test('Positive Login Test', async ({ page }) => {
-//         const username = process.env.USER_NAME;
-//         const password = process.env.PASSWORD;
-        
-//     await page.locator('#username').fill(loginData.username);
-//     await page.locator('#password').fill(loginData.password);
-//     await page.locator('#submit').click();
-
-//     await expect(page).toHaveURL(/.*logged-in-successfully/);
-//     await expect(page.locator('text=successfully logged in')).toBeVisible();
-//     await expect(page.locator('text=Log out')).toBeVisible();
-//     });
+await page.locator('#username').fill(testData.username);
+await page.locator('#password').fill(testData.password);
+await page.locator('#submit').click();
 
 
-// });
+
+
+
+});
+
+
+
+
+
