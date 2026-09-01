3ae36f0a993cd8f218288dbf3cc6763146069f43
sankalpgit97-design <sankalpgit97@gmail.com>
2026-09-01 08:43:07 +0530

    git commit 6.5

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,16 +1,15 @@
 const {test, expect} = require('@playwright/test');
-const loginData=require("../data/loginData.json");
+const  loginData = require('../data/loginData.json');
 
-test('Sucessfull Login Test using JSON Data' ,async ({page}) => {
-await page.goto(loginData.url);
+test('Sucessfull Login Test using JSON Data' ,async ({page,context}) => {
+
+await page.goto(loginData.loginUrl);
 
 await page.locator('#username').fill(loginData.username);
 await page.locator('#password').fill(loginData.password);
 await page.locator('#submit').click();
 
+await expect(page).toHaveURL(loginData.dashboardUrl);
+
+await page.context().storageState({path: '.auth/user.json'});
 });
-
-
-
-
-
